42c78ad3dd5a626b2cba5e57d297995a22b6e25f
Angela Xu <xuangela@cs.washington.edu>
2020-06-23 19:38:54 -0700

    total and tip fade in and out depending on if bill is 0

--- Tipster/ViewController.m
+++ Tipster/ViewController.m
@@ -23,9 +23,23 @@
 - (void)viewDidLoad {
     [self.billField becomeFirstResponder];
     [super viewDidLoad];
+
     // Do any additional setup after loading the view.
     
 }
+
+- (void) viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    //invisible tip and total
+    self.tipLabel.alpha = 0;
+    self.totalLabel.alpha = 0;
+    
+    //tip percentage set to default
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    self.tipControl.selectedSegmentIndex = [defaults doubleForKey:@"default_seg"];
+}
+
 
 - (void) didReeiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -34,6 +48,18 @@
 
 
 - (IBAction)onEdit:(id)sender {
+    if ([self.billField.text doubleValue]==0) {
+        [UIView animateWithDuration:1 animations:^{
+            self.tipLabel.alpha = 0;
+            self.totalLabel.alpha = 0;
+        }];
+    } else {
+        [UIView animateWithDuration:1 animations:^{
+            self.tipLabel.alpha = 1;
+            self.totalLabel.alpha = 1;
+        }];
+    }
+    
     double bill = [self.billField.text doubleValue];
     
     NSArray *percentages = @[@(0.15), @(0.2), @(0.22)];
@@ -47,35 +73,25 @@
     self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
 }
 
-- (IBAction)onEditingBegin:(id)sender {
+
+
+
 //    [UIView animateWithDuration:0.2 animations:^{
 //        self.billField.frame = CGRectMake(self.billField.frame.origin.x, self.billField.frame.origin.y + 30, self.billField.frame.size.width, self.billField.frame.size.height);
 //    }];
-//    
-//    [UIView animateWithDuration:1 animations:^{
-//        self.tipLabel.alpha = 0;
-//    }];
-}
 
-- (IBAction)onEditingEnd:(id)sender {
+
 //    CGRect newFrame = self.billField.frame;
 //    newFrame.origin.y -= 30;
-//    
+//
 //    [UIView animateWithDuration:0.2 animations:^{
 //        self.billField.frame = newFrame;
 //    }];
-//    
+//
 //    [UIView animateWithDuration:1 animations:^{
 //        self.tipLabel.alpha = 1;
 //    }];
     
-}
 
-- (void) viewWillAppear: (BOOL) animated {
-    [super viewWillAppear:animated];
-    
-    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    self.tipControl.selectedSegmentIndex = [defaults doubleForKey:@"default_seg"];
-}
 
 @end
